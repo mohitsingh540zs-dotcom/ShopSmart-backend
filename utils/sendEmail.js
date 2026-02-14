b567cfdfa39e1d967dccc8c37c11373dfaf9b996
MohitSingh <mohitsingh540zs@gmail.com>
2026-02-15 01:46:47 +0530

    Installed new Brevo SDK

---
+++ utils/sendEmail.js
@@ -0,0 +1,33 @@
+import Brevo from "@getbrevo/brevo";
+
+const sendEmail = async ({ to, subject, html }) => {
+  if (!process.env.BREVO_API_KEY) {
+    throw new Error("BREVO_API_KEY not found in environment variables");
+  }
+
+  const apiInstance = new Brevo.TransactionalEmailsApi();
+
+  apiInstance.setApiKey(
+    Brevo.TransactionalEmailsApiApiKeys.apiKey,
+    process.env.BREVO_API_KEY
+  );
+
+  try {
+    const response = await apiInstance.sendTransacEmail({
+      sender: {
+        email: "mohitsingh540zs@gmail.com",
+        name: "Shopsmart",
+      },
+      to: [{ email: to }],
+      subject: subject,
+      htmlContent: html,
+    });
+
+    console.log("Email sent successfully:", response.messageId);
+  } catch (error) {
+    console.error("Brevo Error:", error.response?.body || error);
+    throw new Error("Email sending failed");
+  }
+};
+
+export default sendEmail;
