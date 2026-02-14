6930b2a23a983b845d13861df9464669ce5c01c1
MohitSingh <mohitsingh540zs@gmail.com>
2026-02-14 11:10:24 +0530

    Mailer changes

--- config/Mailer.js
+++ config/Mailer.js
@@ -4,6 +4,9 @@ import nodemailer from "nodemailer";
 export const getTransporter = () => {
     return nodemailer.createTransport({
         service: "gmail",
+        host: "smtp.gmail.com",
+        port: 465,
+        secure:true,
         auth: {
             user: process.env.MAIL_USER,
             pass: process.env.MAIL_PASS
